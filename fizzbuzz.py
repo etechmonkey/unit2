ec32d87470cb00dda02d631e2dc2bdce5eb5ad65
etechmonkey <deannatzhang@gmail.com>
2022-01-12 10:51:22 -0600

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+for n in range(1,1001):
+    if((n%3==0) & (n%5==0)):
+        a="FizzBuzz"
+    elif((n%3==0) & (n%5!=0)):
+        a="Fizz"
+    elif((n%5==0) & (n%3!=0)):
+        a="Buzz"
+    else:
+        a=n
+    print(a)
